e3bbf13b1c694dd14052b8565109708c8d70e94b
zimrustpro <zimrustpro@gmail.com>
2024-11-22 22:13:40 +0200

    Return a &str with a static lifetime

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ fn prints_str(my_str: &str) {
     println!("{}", my_str);
 }
 
+fn works() -> &'static str {
+    "I live forever"
+}
+
 fn main() {
     let my_string = String::from("I am a string");
     prints_str(&my_string);
+    works();
 }
